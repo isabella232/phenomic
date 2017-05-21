c70350194c8f6ff71f06f439145b2232a1ba02aa
davidwells <david@inboundnow.com>
2017-05-20 18:29:24 -0700

    disable sw reload if config set

--- src/_utils/offline/runtime.js
+++ src/_utils/offline/runtime.js
@@ -5,6 +5,7 @@
 import offlinePluginRuntime from "offline-plugin/runtime"
 
 console.log("SW Event:", "Installing")
+console.log('process.env.PHENOMIC_DISABLE_SW_RELOAD', process.env.PHENOMIC_DISABLE_SW_RELOAD)
 offlinePluginRuntime.install({
   // you can specify here some code to respond to events
   // see here for more informations
@@ -21,7 +22,20 @@ offlinePluginRuntime.install({
   },
   onUpdated: () => {
     console.log("SW Event:", "onUpdated")
-    window.location.reload()
+    if (process.env.PHENOMIC_DISABLE_SW_RELOAD) {
+      console.log("stop hard reload")
+      const anchors = document.getElementsByTagName("a");
+      for (var i = 0; i < anchors.length; i++) {
+        anchors[i].onclick = function(e) {
+          e.preventDefault()
+          // stop react router and just hard reload the clicked href to get new contents
+          window.location.href = e.target.href
+          return false;
+        };
+      }
+    } else {
+      window.location.reload()
+    }
   },
   onUninstalled: () => {
     console.log("SW Event:", "onUninstalled")
